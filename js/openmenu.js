19af42aa2acd22947888d72536950bf1d5a35270
peppemete@Home <58082413+peppemete@users.noreply.github.com>
2020-04-25 19:38:21 +0200

    hamburger menu added

---
+++ js/openmenu.js
@@ -0,0 +1,38 @@
+var aside = document.getElementById("aside");
+
+aside.innerHTML = `
+        <div class="menuBtn" id="menuBtn">
+            <div class="bar1"></div>
+            <div class="bar2"></div>
+            <div class="bar3"></div>
+            </div>
+            <div class="logo">
+                <a href="index.html" class="logo-home">
+                    <h1>giemme</h1>
+                    <h5>coder & designer</h5>
+                    </a>
+                    </div>
+                    <h5 class="goback"><a  href="javascript:history.back()">go back</a> </h5>
+                    <div id="menuWindow" class="menu-window">
+                        <ul>
+                            <li><h2><a href="index.html">Home</a></h2></li>
+                            <li><h2><a href="projects.html">Project</a></h2></li>
+                            <li><h2><a href="about.html">About</a></h2></li>
+                            <li><h2><a href="contact.html">Contact</a></h2></li>
+                            </ul>
+                            </div>
+                            <div id="menuBg" class="menubg"></div>
+                            `;
+var menuBtn=document.getElementById("menuBtn");
+var bars=menuBtn.querySelectorAll("div");
+var menuWindow=document.getElementById("menuWindow");
+var menuBg=document.getElementById("menuBg");
+menuBtn.addEventListener("click",openMenu)
+function openMenu() {
+    console.log("you clicked");
+    menuWindow.classList.toggle("menu-window-open");
+    menuBg.classList.toggle("menubg-visible");
+    bars.forEach(bar => bar.classList.toggle("change"))
+    aside.classList.toggle("open");
+
+}
